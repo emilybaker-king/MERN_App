56fe4c7a447d037d00b2e5706ff17381a1798680
Emily Baker-King <ebakerking@gmail.com>
2019-11-25 10:13:29 -0600

    Added in gitignore to ignore node modules. Installed packages for server

--- backend/secret.js
+++
@@ -1,9 +0,0 @@
-
-const secrets = {
-    //The URL that we use to connect to the MongoDB Atlas Cluster
-    dbUri: 'mongodb+srv://ebakerking:password!123@cluster0-ozvui.mongodb.net/test?retryWrites=true&w=majority'
-};
-
-const getSecret = key => secrets[key];
-
-module.exports = getSecret;
--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,4 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
